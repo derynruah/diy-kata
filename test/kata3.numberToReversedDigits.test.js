42ed12f475d377eb9dd84fb4c657b42678364ee2
Deryn Rushworth <derynrushworth@gmail.com>
2022-08-08 16:02:35 +0100

    completed numberToReversedDigit

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,9 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  it("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(1234)).toStrictEqual([ 4, 3, 2, 1 ]);
+    expect(numberToReversedDigits(2468)).toStrictEqual([ 8, 6, 4, 2 ]);
+    expect(numberToReversedDigits(112358)).toStrictEqual([ 8, 5, 3, 2, 1, 1]);
+  });
 });
