97114d2e2ed8f806dfde007c9772eab0c4c133ba
Deryn Rushworth <derynrushworth@gmail.com>
2022-08-10 16:53:34 +0100

    completed humanCatDogYears test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,5 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+return [number, 24+(4*(number-2)), 24+(5*(number-2))];
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+  it("returns array of human, cat and dog years when passed human years", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(20)).toEqual([20, 96, 114]);
+    expect(humanCatDogYears(4)).toEqual([4, 32, 34]);
+  });
+});
